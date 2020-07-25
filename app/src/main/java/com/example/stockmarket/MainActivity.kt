8e68fabffa7dd672bb951dfaf0528547a91e1424
Carmelo Iriti <ciriti@carmelos-mbp.localdomain>
2020-07-25 16:37:10 +0200

    fixed margin

--- app/src/main/java/com/example/stockmarket/MainActivity.kt
+++ app/src/main/java/com/example/stockmarket/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.stockmarket.core.init
 import com.example.stockmarket.ui.stockprice.StockPriceFragment
-import com.example.stockmarket.ui.stockprice.StockPriceViewModel
-import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private val vm by viewModel<StockPriceViewModel>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

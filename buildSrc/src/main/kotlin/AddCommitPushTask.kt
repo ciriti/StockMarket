e2be99a5dc6fce57416fac9b596436327e5a5389
GitHub <ciriti@gmail.com>
2021-01-11 00:10:33 +0100

    added github user and email

--- buildSrc/src/main/kotlin/AddCommitPushTask.kt
+++ buildSrc/src/main/kotlin/AddCommitPushTask.kt
@@ -25,8 +25,6 @@ open class AddCommitPushTask : DefaultTask() {
         (project.properties["user_email"] as? String) ?: ""
     }
 
-    private val filesList: List<String> by lazy { filesArg.split(":") }
-
     @TaskAction
     fun execute() {
         addCommitPush()
@@ -37,6 +35,7 @@ open class AddCommitPushTask : DefaultTask() {
         "git config user.email $userEmail".runCommand(workingDir = project.rootDir)
         "git config user.name $userName".runCommand(workingDir = project.rootDir)
         "git pull".runCommand(workingDir = project.rootDir)
+        val filesList= filesArg.split(":")
         filesList.forEach {
             "echo $it".runCommand(workingDir = project.rootDir)
             "git add $it".runCommand(workingDir = project.rootDir)
